91e2859280da2588295c278af8528aad76727a78
Jan Remiel Menor <78302040+menorjanremiel@users.noreply.github.com>
2022-05-06 22:35:36 +0800

    Initial commit

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-header">
+      Midterm Hands on Exam
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Jan Remiel Menor</h5>
+      <p class="card-text">Welcome to my Web Home Page</p>
+      <a href="#" class="btn btn-primary">Go to my repository</a>
+    </div>
+  </div>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('content')
+    about page
+    
+@endsection
